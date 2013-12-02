312d5288315a0f228bf4ce4a55976d78a8f198bd
Rob M Frawley 2nd <rfrawley@scribenet.com>
2013-12-02 09:59:04 -0500

    removed unneeded white-space

--- ScribeSwimBundle.php
+++ ScribeSwimBundle.php
@@ -1,9 +1,15 @@
 <?php
+/*
+ * This file is part of the Scribe World Application.
+ *
+ * (c) Scribe Inc. <scribe@scribenet.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Scribe\SwimBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class ScribeSwimBundle extends Bundle
-{
-}
+class ScribeSwimBundle extends Bundle {}
